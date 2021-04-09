5b8b85163d2dcf592c885327a5b70c2081af08da
YuTaeKim <yek7883@naver.com>
2021-04-09 11:46:16 +0900

    Revert "Test3"
    This reverts commit 709b30486f696e7eded157a4d23073998e9ec898.

--- containers/Test/index.tsx
+++ containers/Test/index.tsx
@@ -11,24 +11,7 @@ import styled from 'styled-components';
 const TestContainer = () => {
   return (
     <Container>
-      <Grid>aaa</Grid>
-      <Grid>bbb</Grid>
-      <Grid>ccc</Grid>
-      <Grid>ddd</Grid>
-      <Grid>eee</Grid>
-      <Grid>aaa</Grid>
-      <Grid>bbb</Grid>
-      <Grid>ccc</Grid>
-      <Grid>ddd</Grid>
-      <Grid>eee</Grid> <Grid>aaa</Grid>
-      <Grid>bbb</Grid>
-      <Grid>ccc</Grid>
-      <Grid>ddd</Grid>
-      <Grid>eee</Grid> <Grid>aaa</Grid>
-      <Grid>bbb</Grid>
-      <Grid>ccc</Grid>
-      <Grid>ddd</Grid>
-      <Grid>eee</Grid>
+      sssss<button onClick={() => Router.push('/')}>click</button>
     </Container>
   );
 };
@@ -37,18 +20,5 @@ export default TestContainer;
 
 const Container = styled.div<any>`
   height: 100vh;
-  display: grid;
-  grid-template-columns: repeat(5, 200px);
-  grid-template-rows: repeat(4, 200px);
-  justify-content: space-around;
-  align-content: space-around;
+  overflow-y: scroll;
 `;
-
-const Grid = styled.div`
-  background-color: pink;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-//dddd
